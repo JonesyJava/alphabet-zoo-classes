8707ff6651ba1ca42041e0a4f96d56b62d697c79
jonesyjava <jonesyjava@gmail.com>
2021-02-15 19:17:38 -0700

    finished day 9 Alphabet Zoo Challenge

---
+++ app.js
@@ -0,0 +1,342 @@
+class Alligator{
+    constructor(name, species, color, gender, weight, noise){
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.noise = noise;
+    }
+
+}
+
+let Ally = new Alligator("ally", "reptile", "green","male", "heavy", "RAWR")
+
+class Bear{
+    constructor(name, species, color, gender, weight, noise, region){
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.noise = noise;
+        this.region = region;
+    }
+}
+
+let Barry = new Bear("Barry", "mammal", "black", "male", "heavy", "RAWR", "North America")
+
+class Camel{
+    constructor(name, species, gender, weight, region){
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.weight = weight;
+        this.region = region;
+    }
+}
+
+let Cami = new Camel("Cami", "mammal", "female", "heavy", "Asia")
+
+class Dragon{
+    constructor(name, region, gender, weight, flight, dragon, species){
+        this.name = name;
+        this.region = region;
+        this.gender = gender;
+        this.weight = weight;
+        this.flight = flight;
+        this.dragon = dragon;
+        this.species = species;
+    }
+}
+
+let Drako = new Dragon("Drako", "Asia", "male", "heavy", "flies", "dragon", "reptile")
+
+class Elephant{
+    constructor(name, species, gender, region, weight, tusks){
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.region = region;
+        this.weight = weight;
+        this.tusks = tusks;
+    }
+}
+
+let Emmy = new Elephant("Emmy", "mammal", "female", "Africa", "heavy", "big tusks")
+
+class Fox{
+    constructor(name, species, gender, pet, tail){
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.pet = pet;
+        this.tail = tail;
+    }
+}
+
+let Flinn = new Fox("Flinn", "mammal", "male", "my pet", "bushy tail")
+
+class Gorilla{
+    constructor(name, species, region, color, gender, weight){
+        this.name = name;
+        this.species = species;
+        this.region = region;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+    }
+}
+
+let Kong = new Gorilla("Kong", "mammal", "Africa", "black", "male", "heavy")
+
+class Hydra{
+    constructor(name, heads, swim, loch, enemy, flight, species){
+        this.name = name;
+        this.heads = heads;
+        this.swim = swim;
+        this.loch = loch;
+        this.enemy = enemy;
+        this.flight = flight;
+        this.species = species;
+    }
+}
+
+let Hydro = new Hydra("Hydro", "8", "swims", "Loch Ness", "enemy of Hercules", "flies whenever it wants", "reptile")
+
+class Iara{
+    constructor(name, region, swim, spiritGender){
+        this.name = name;
+        this.region = region;
+        this.swim = swim;
+        this.spiritGender = spiritGender;
+    }
+}
+
+let Ivy = new Iara ("Ivy", "Asia", "swims fast", "female")
+
+class Jaguar{
+    constructor(name, species, gender, spiritType, region){
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.spiritType = spiritType;
+        this.region = region;
+    }
+}
+
+let Jazzy = new Jaguar ("Jazzy", "mammal", "female", "warrior", "South America")
+
+class Kachina{
+    constructor(name, region, swim, spiritType, culture){
+        this.name = name;
+        this.region = region;
+        this.swim = swim;
+        this.spiritType = spiritType; 
+        this.culture = culture;
+    }
+}
+
+let Kami = new Kachina ("Kami", "North America", "swims fast", "Ancestral", "Pueblo")
+
+class Labbu{
+    constructor(name, swim, species, region, enemy){
+        this.name = name;
+        this.swim = swim;
+        this.species = species;
+        this.region = region;
+        this.enemy = enemy;
+    }
+}
+
+let Lacy = new Labbu ("Lacy", "swims fast", "reptile", "Asia", "enemy of humans")
+
+class Madusa{
+    constructor(name, gender, region, enemy, power){
+        this.name = name;
+        this.gender = gender;
+        this.region = region;
+        this.enemy = enemy;
+        this.power = power
+    }
+}
+
+let Maddi = new Madusa ("Maddi", "female", "Europe", "enemy of humans", "turns people to stone")
+
+class Nakki{
+    constructor(name, spirit, region, swim){
+        this.name = name;
+        this.spirit = spirit;
+        this.region = region;
+        this.swim = swim;
+    }
+}
+
+let Nakole = new Nakki ("Nakole", "water spirit", "Asia", "swims fast")
+
+class Obariyon{
+    constructor(name, region, culture, enemy, spirit, weight){
+        this.name = name;
+        this.region = region;
+        this.culture = culture;
+        this.enemy = enemy;
+        this.spirit = spirit;
+        this.weight = weight;
+    }
+}
+
+let Obi = new Obariyon ("Obi", "Asia", "Japanese", "enemy of humans", "Ghoul", "heavy")
+
+class Pegasus{
+    constructor(name, flight, culture, species, hero){
+        this.name = name;
+        this.flight = flight;
+        this.culture = culture;
+        this.species = species;
+        this.hero = hero;
+    }
+}
+
+let Peggy = new Pegasus ("Peggy", "flies fast", "Greek", "mammal", "Hercules")
+
+class Qilin{
+    constructor(name, culture, hybrid, flight, region){
+        this.name = name;
+        this.cutlture = culture;
+        this.hybrid = hybrid;
+        this.flight = flight;
+        this.region = region;
+    }
+}
+
+let Qilly = new Qilin ("Qilly", "Chinese", "Dragon-Ox-Deer", "flies fast", "Asia")
+
+class RainbowSerpent{
+    constructor(name, species, culture, region, spirit){
+        this.name = name
+        this.species = species;
+        this.culture = culture;
+        this.region = region;
+        this.spirit = spirit;
+    }
+}
+
+let Rainy = new RainbowSerpent ("Rainy", "reptile", "Aboriginal", "Australian", "lucky spririt")
+
+class Saci{
+    constructor(name, spiritType, region, culture, leg){
+        this.name = name;
+        this.spiritType = spiritType;
+        this.region = region;
+        this.culture = culture;
+        this.leg = leg;
+    }
+}
+
+let Stacy = new Saci ("Stacy", "Nature", "South America", "Brazilian", "one-legged")
+
+class Tailypo{
+    constructor(name, culture, tail, powerful, region){
+        this.name = name;
+        this.culture = culture;
+        this.tail = tail;
+        this.powerful = powerful;
+        this.region = region;
+    }
+}
+
+let Taily = new Tailypo ("Taily", "Appalachia", "stealable tail", "super strength", "North America")
+
+class Ubeme{
+    constructor(name, gender, culture, spirit){
+        this.name = name;
+        this.gender = gender;
+        this.culture = culture;
+        this.spirit = spirit;
+    }
+}
+
+let Ubba = new Ubeme ("Ubba", "female", "Japanese", "ghost")
+
+class  Valravn{
+    constructor(name, spiritType, species, flight, culture){
+        this.name = name;
+        this.spiritType = spiritType;
+        this.species = species;
+        this.flight = flight;
+        this.culture = culture;
+    }
+}
+
+let Valvee = new Valravn ("Valvee", "ghost", "bird", "flies fast", "Danish")
+
+class WinniePooh{
+    constructor(name, species, color, culture){
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.culture = culture;
+    }
+}
+
+let Pooh = new WinniePooh ("Pooh", "mammal", "yellow", "American")
+
+class Xana{
+    constructor(name, culture, spiritType, gender){
+        this.name = name;
+        this.culture = culture;
+        this.spiritType = spiritType
+        this.gender = gender;
+    }
+}
+
+let Xena = new Xana ("Xena", "Asturian", "water spirit", "female")
+
+class Yacumama{
+    constructor(name, region, culture, swim, enemy){
+        this.name = name;
+        this.region = region;
+        this.culture = culture;
+        this.swim = swim;
+        this.enemy = enemy;
+    }
+}
+
+let Yacu = new Yacumama ("Yacu", "South America", "Aztec", "swims fast", "enemy of humans")
+
+class Zahhak{
+    constructor(name, culture, dragon, species){
+        this.name = name;
+        this.culture = culture;
+        this.dragon = dragon;
+        this.species = species;
+    }
+}
+
+let Zak = new Zahhak ("Zak", "Persian", "dragon", "reptile")
+
+
+let mammals = [Barry, Cami, Emmy, Flinn, Kong, Jazzy, Peggy, Pooh];
+    console.log(mammals);
+
+let reptile = [Ally, Drako, Hydro, Lacy, Rainy, Zak];
+    console.log(reptile);
+
+let southAmerica = [Yacu, Stacy, Jazzy];
+    console.log(southAmerica);
+
+let northAmerica = [Barry, Kami, Taily];
+    console.log(northAmerica);
+
+let asia = [Nakole, Cami, Drako, Ivy, Lacy, Obi, Qilly];
+    console.log(asia);
+
+let dragons = [Drako, Zak];
+    console.log(dragons);
+
+let spirit = [Ivy, Jazzy, Kami, Lacy, Nakole, Obi, Rainy, Stacy, Ubba, Valvee, Xena];
+    console.log(spirit);
+
+let swimmer = [Hydro, Ivy, Kami, Lacy, Nakole, Yacu];
+    console.log(swimmer);
+
